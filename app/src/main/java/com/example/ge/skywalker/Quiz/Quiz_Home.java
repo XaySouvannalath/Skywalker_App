9bed4622f165455373d67926dc1af79c14e55c20
XaySouvannalath <saiyavongmitthasone@gmail.com>
2018-01-22 15:33:28 +0700

    Rejected commit: Default

---
+++ app/src/main/java/com/example/ge/skywalker/Quiz/Quiz_Home.java
@@ -0,0 +1,46 @@
+package com.example.ge.skywalker.Quiz;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import com.example.ge.skywalker.Ac_Coursetypes;
+import com.example.ge.skywalker.R;
+import com.example.ge.skywalker.Skybase_Home;
+
+public class Quiz_Home extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_quiz__home);
+        getSupportActionBar();
+    }
+
+    public void gohome(MenuItem item) {
+        Intent i = new Intent(this, Skybase_Home.class);
+        startActivity(i);
+    }
+
+    public void gosetting(MenuItem item) {
+        Intent i = new Intent(this, Ac_Coursetypes.class);
+        startActivity(i);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if(item.getItemId()== R.id.action_login){
+            //  Toast.makeText(this, "Add Click", Toast.LENGTH_SHORT).show();
+            //   Intent intent = new Intent(this, Ac_Headings.class);
+            //   startActivity(intent);
+        }
+        return super.onOptionsItemSelected(item);
+    }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.home_menu, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+}
